96292c20774deae36369f9410c13e8afd90b4f2f
Edwin Allauca <edwindavid21v@gmail.com>
2025-02-02 23:36:10 -0500

    Add new files and jwt configurations. (#4)
    * feat: :wrench: Add files
    
    Add files for get rep for inventory management
    
    * feat: :wrench: Add dockerfile and yml
    
    Add new dockerfile file and yml file
    
    * feat: :wrench: Add new files
    
    Add files for swaggers and updates other files
    
    * fix: :bug: Fix ci.yml file
    
    Add lines of code in ci.yml and update workflow
    
    * feat: :memo: Add readme
    
    Add the repository usage readme.
    
    * feat: :sparkles: New features in inventoryController
    
    Add new files for select mariadb
    
    * feat: :sparkles: Add new files and jwt
    
    Add new files for middlewares and jwt configurations

--- src/routes/inventoryRoutes.js
+++ src/routes/inventoryRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { getSpecificInventory } = require('../controllers/inventoryController');
+const { authorize } = require('../Middlewares/authMiddleware');
 
 const router = express.Router();
 
 // Route to obtain medicines with specific fields
-router.get('/specific', getSpecificInventory);
+router.get('/specific', getSpecificInventory, authorize);
 
 module.exports = router;
