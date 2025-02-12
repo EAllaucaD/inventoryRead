8d4e1cc46162dec83abfc161ff02312f1edb71cf
Edwin Allauca <edwindavid21v@gmail.com>
2025-02-12 00:19:36 -0500

    Fix a bug in yml. (#7)
    * feat: :wrench: Add files
    
    Add files for get rep for inventory management
    
    * feat: :wrench: Add dockerfile and yml
    
    Add new dockerfile file and yml file
    
    * feat: :wrench: Add new files
    
    Add files for swaggers and updates other files
    
    * fix: :bug: Fix ci.yml file
    
    Add lines of code in ci.yml and update workflow
    
    * feat: :memo: Add readme
    
    Add the repository usage readme.
    
    * feat: :sparkles: New features in inventoryController
    
    Add new files for select mariadb
    
    * feat: :sparkles: Add new files and jwt
    
    Add new files for middlewares and jwt configurations
    
    * feat: :sparkles: Add new configurations
    
    Add new configurations in yml
    
    * feat: :bug: Fix a bug in yml
    
    Update new changes in yml
    
    * feat: :bug: Fix a bug
    
    Fix a bug in yml
    
    * feat: :wrench: Update configurations
    
    Update configurations
    
    * feat: :wrench: Update configurations
    
    Update configurations for db
    
    * feat: :wrench: Update configurations
    
    Update new configurations
    
    * feat: :bug: Fix a bug
    
    * feat: :bug: Fix a bug
    
    Fix a bug
    
    * feat: :sparkles: Add configurations for prometheus and grafana
    
    Add configurations for prometheus and grafana
    
    * feat: :bug: Fix a bug.
    
    Fix a bug.
    
    * feat: :bug: Fix a bug
    
    Fix a bug

--- src/app.js
+++ src/app.js
@@ -4,12 +4,64 @@ const YAML = require('yaml');
 const fs = require('fs');
 const path = require('path');
 const swaggerUi = require('swagger-ui-express');
+const client = require('prom-client');
+
 require('dotenv').config();
 
 const inventoryRoutes = require('./routes/inventoryRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3012;
+const port = process.env.PORT || 3012;
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+
+// Configuring Prometheus metrics
+collectDefaultMetrics();
+
+// Request counter
+const httpRequestCounter = new client.Counter({
+  name: "http_requests_total",
+  help: "Total HTTP requests received",
+  labelNames: ["method", "route", "status_code"],
+});
+
+// Histogram for response times
+const httpRequestDuration = new client.Histogram({
+  name: "http_request_duration_seconds",
+  help: "Histogram for the duration of HTTP requests in seconds",
+  labelNames: ["method", "route", "status_code"],
+  buckets: [0.1, 0.5, 1, 2, 5],
+});
+
+//MMiddleware to measure metrics on each request
+app.use((req, res, next) => {
+  const start = Date.now();
+
+  res.on("finish", () => {
+    const duration = (Date.now() - start) / 1000;
+
+    httpRequestCounter.inc({
+      method: req.method,
+      route: req.path,
+      status_code: res.statusCode,
+    });
+
+    httpRequestDuration.observe(
+      { method: req.method, route: req.path, status_code: res.statusCode },
+      duration
+    );
+  });
+
+  next();
+});
+
+// Metrics endpoint
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
+
 
 // Middleware
 app.use(cors());
@@ -29,7 +81,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Error on the server.' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server run in http://localhost:${PORT}`);
-  console.log(`Swagger docs available at http://localhost:${PORT}/api-docs`);
+app.listen(port, () => {
+  console.log(`Server run in http://localhost:${port}`);
+  console.log(`Swagger docs available at http://localhost:${port}/api-docs`);
 });
